3616a4cec30d3b69e4ddf36133a0553013e8d64f
ChabinDev <142836013+ChabinDev@users.noreply.github.com>
2024-03-26 09:21:47 +0100

    Update Error.js

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -5,8 +5,10 @@ const Error = () => {
   return (
     <div className='content-error'>
       <h1 className='h1-error'>404</h1>
+    <div className='content-error-first'>
       <h2 className='h2-error'>Oups! La page que vous demandez n'existe pas.</h2>
-      <NavLink to="/">
+    </div>
+      <NavLink to="/P8_Kasa/">
           <p className='p-error'>Retourner sur la page d'accueil</p>
       </NavLink>
       </div>
