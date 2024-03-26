b71d3ade3478daea89551a22488812102b597c52
ChabinDev <chabin_972@hotmail.fr>
2024-03-26 23:02:43 +0100

    Published

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,30 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import Logo from '../assets/img/Logo_header.png'
 
-
 const Header = () => {
   return (
-      <header className='content-header'>
-        <img src={Logo} className='header_logo'  alt='Logo_header' />
-        <nav className='nav-header'>
-            <ul className='ul-header'>
-                <li className='li-header'>
-                    <NavLink to="/" className={nav => nav.isActive ? "nav-active" : ""}>Accueil</NavLink>
-                </li>
-                <li className='li-header'>
-                    <NavLink to="/a_propos" className={nav => nav.isActive ? "nav-active" : "" }>A propos</NavLink>
-                </li>
-            </ul>
-        </nav>
+    <header className="content-header">
+      <img src={Logo} className="header_logo" alt="Logo_header" />
+      <nav className="nav-header">
+        <ul className="ul-header">
+          <li className="li-header">
+            <NavLink
+              to="/P8_Kasa"
+              className={(nav) => (nav.isActive ? 'nav-active' : '')}
+            >
+              Accueil
+            </NavLink>
+          </li>
+          <li className="li-header">
+            <NavLink
+              to="/P8_Kasa/a_propos"
+              className={(nav) => (nav.isActive ? 'nav-active' : '')}
+            >
+              A propos
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
