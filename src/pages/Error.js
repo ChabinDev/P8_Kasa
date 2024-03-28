9cb8ac2c7e3d5ce8d131b496eb3bf7bf724ffa8f
ChabinDev <chabin_972@hotmail.fr>
2024-03-28 17:05:40 +0100

    Published

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -3,15 +3,17 @@ import { NavLink } from 'react-router-dom'
 
 const Error = () => {
   return (
-    <div className='content-error'>
-      <h1 className='h1-error'>404</h1>
-    <div className='content-error-first'>
-      <h2 className='h2-error'>Oups! La page que vous demandez n'existe pas.</h2>
-    </div>
-      <NavLink to="/P8_Kasa/">
-          <p className='p-error'>Retourner sur la page d'accueil</p>
-      </NavLink>
+    <div className="content-error">
+      <h1 className="h1-error">404</h1>
+      <div className="content-error-first">
+        <h2 className="h2-error">
+          Oups! La page que vous demandez n'existe pas.
+        </h2>
       </div>
+      <NavLink to="/">
+        <p className="p-error">Retourner sur la page d'accueil</p>
+      </NavLink>
+    </div>
   )
 }
 
