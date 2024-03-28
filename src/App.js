d6937ab98293439266c4efcb6474f5e0d4b383ba
ChabinDev <chabin_972@hotmail.fr>
2024-03-28 16:16:31 +0100

    Published

--- src/App.js
+++ src/App.js
@@ -12,14 +12,10 @@ const App = () => {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/P8_Kasa" element={<Home />} />
+        <Route exact path="/P8_Kasa" element={<Home />} />
         <Route path="/logement/:id" element={<Logement />} />
         <Route path="/P8_Kasa/a_propos" element={<Apropos />} />
-<<<<<<< HEAD
         <Route path="*" element={<Error />} />
-=======
-        <Route path="*/*" element={<Error />} />
->>>>>>> 1a8dfac9f55c05fee8fe8116ba6d7ef37fbce386
       </Routes>
       <Footer />
     </BrowserRouter>
